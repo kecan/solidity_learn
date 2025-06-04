0779c779ba0b97481dcaf0efb56c6b4c2605902b
kecan <1905537268@qq.com>
2025-06-04 22:59:35 +0800

    创建主应用组件

---
+++ client/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { WagmiProvider } from 'wagmi'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ConnectKitProvider } from 'connectkit'
+import config from './config/wagmi'
+import RedPacketDApp from './components/RedPacketDApp'
+
+const queryClient = new QueryClient()
+
+function App() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider 
+          theme="auto"
+          options={{
+            embedGoogleFonts: true,
+            hideBalance: false,
+            hideTooltips: false,
+            hideQuestionMarkCTA: false,
+            hideNoWalletCTA: false,
+            walletConnectCTA: "both",
+            enforceSupportedChains: true,
+            language: "zh-CN",
+          }}
+        >
+          <div className="container">
+            <div className="header">
+              <h1>🧧 红包 DApp</h1>
+              <p>基于区块链的去中心化红包系统 - React 版本</p>
+            </div>
+            <RedPacketDApp />
+          </div>
+        </ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+export default App
